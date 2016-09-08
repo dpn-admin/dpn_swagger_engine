fb97f9450c15fa84cf81c7d9c9ee379a17cd7415
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2016-09-08 10:47:37 -0700

    Update assets

--- lib/dpn_swagger_engine/engine.rb
+++ lib/dpn_swagger_engine/engine.rb
@@ -2,20 +2,32 @@ module DpnSwaggerEngine
   class Engine < ::Rails::Engine
     isolate_namespace DpnSwaggerEngine
 
-    #https://gist.github.com/parndt/11381872
-=begin
-    initializer "dpn_swagger_engine.assets.precompile", group: :all do |app|
+    initializer :assets do |app|
+      # Rails.application.config.assets.precompile += [
       app.config.assets.precompile += [
         'dpn_swagger_engine/print.css',
-        'dpn_swagger_engine/reset.css'
+        'dpn_swagger_engine/reset.css',
+        # fonts
+        'dpn_swagger_engine/DroidSans.ttf',
+        'dpn_swagger_engine/DroidSans-Bold.ttf',
+        # images
+        'dpn_swagger_engine/collapse.gif',
+        'dpn_swagger_engine/expand.gif',
+        'dpn_swagger_engine/explorer_icons.png',
+        'dpn_swagger_engine/favicon-16x16.png',
+        'dpn_swagger_engine/favicon-32x32.png',
+        'dpn_swagger_engine/favicon.ico',
+        'dpn_swagger_engine/logo_small.png',
+        'dpn_swagger_engine/pet_store_api.png',
+        'dpn_swagger_engine/throbber.gif',
+        'dpn_swagger_engine/wordnik_api.png'
       ]
-    end
-=end
-    config.to_prepare do
-      Rails.application.config.assets.precompile += [
-        'dpn_swagger_engine/print.css',
-        'dpn_swagger_engine/reset.css'
-      ]
+      app.config.assets.paths << root.join(
+        'app', 'assets', 'images', 'dpn_swagger_engine', 'dpn_swagger_engine'
+      )
+      app.config.assets.paths << root.join(
+        'app', 'assets', 'fonts', 'dpn_swagger_engine', 'dpn_swagger_engine'
+      )
     end
   end
 
